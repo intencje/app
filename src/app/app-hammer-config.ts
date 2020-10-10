c81a1b1145fe001bc30f4cbb0eb26d144952390d
Sławomir Jakubowski <jakubowski@slawomir.info>
2020-10-10 07:46:04 +0200

    IN-129, Switch to Yarn

---
+++ src/app/app-hammer-config.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HammerGestureConfig } from '@angular/platform-browser';
+
+@Injectable()
+export class AppHammerConfig extends HammerGestureConfig {
+  overrides = <any>{
+    swipe: { direction: 6 },
+    pinch: { enable: false },
+    rotate: { enable: false },
+    pan: { enable: false },
+  };
+
+  options = {
+    cssProps: {
+      userSelect: 'auto',
+    },
+  };
+}
--- src/app/help/help.module.ts
+++ src/app/help/help.module.ts
@@ -2,12 +2,9 @@ import { NgModule } from '@angular/core';
 import { HelpPageRoutingModule } from './help-routing.module';
 import { HelpPage } from './help.page';
 import { ComponentsModule } from '../_components/components.module';
-import { DirectivesModule } from '../_directives/directives.module';
-import 'hammerjs';
-import { HammerModule } from '@angular/platform-browser';
 
 @NgModule({
-  imports: [HelpPageRoutingModule, ComponentsModule, HammerModule, DirectivesModule],
+  imports: [HelpPageRoutingModule, ComponentsModule],
   declarations: [HelpPage],
 })
 export class HelpPageModule {}
