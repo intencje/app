3d190b7ff83ca4b3ba04c7ccdfa97b4a31164832
Sławomir Jakubowski <jakubowski@slawomir.info>
2021-04-03 11:57:49 +0200

    #137

--- src/app/readings/readings.page.ts
+++ src/app/readings/readings.page.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
 import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
 import { SeoService } from '../_services/seo/seo.service';
+
+export interface Item {
+  name: string;
+}
 
 @Component({
   selector: 'app-readings',
@@ -8,13 +14,16 @@ import { SeoService } from '../_services/seo/seo.service';
   styleUrls: ['./readings.page.scss'],
 })
 export class ReadingsPage implements OnInit {
-
+  private itemDoc: AngularFirestoreDocument<Item>;
+  item: Observable<Item>;
   constructor(
+    private afs: AngularFirestore,
     public readonly route: ActivatedRoute,
-    private readonly seoService: SeoService
-  ) { }
-
-  ngOnInit() {
+    private readonly seoService: SeoService,
+  ) {
+    this.itemDoc = afs.doc<Item>('readings/data');
+    this.item = this.itemDoc.valueChanges();
   }
 
+  ngOnInit() {}
 }
